bb407574014c3b0798ceb799dfb54d42b252e002
khum0r <domagoj.kosmat@gmail.com>
2018-08-14 00:14:00 +0200

    Add files via upload

--- 150-install-fonts-v1.sh
+++ 150-install-fonts-v1.sh
@@ -5,7 +5,7 @@ echo "#################################################"
 echo "Installing fonts..."
 echo "#################################################"
 
-LIST_OF_APPS="noto-fonts-ttf-extra google-fonts-ttf font-fira-ttf font-awesome5 font-iosevka"
+LIST_OF_APPS="noto-fonts-ttf-extra google-fonts-ttf font-awesome5 font-iosevka"
 
 sudo xbps-install -S
 sudo xbps-install -Sy $LIST_OF_APPS
