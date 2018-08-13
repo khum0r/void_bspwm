2cfc521394ef510d211687a5af60a2536df44b44
khum0r <domagoj.kosmat@gmail.com>
2018-08-13 04:16:45 +0200

    Update 20-nobeep-modprobe-v1.sh

--- 20-nobeep-modprobe-v1.sh
+++ 20-nobeep-modprobe-v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
+echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf > /dev/null
 cat /etc/modprobe.d/nobeep.conf
