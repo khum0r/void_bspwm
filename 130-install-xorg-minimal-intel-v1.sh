d7c1b7e392c4cece6e57eff9e9dbf6943963b085
khum0r <domagoj.kosmat@gmail.com>
2018-08-18 13:46:07 +0200

    Update 130-install-xorg-minimal-intel-v1.sh

--- 130-install-xorg-minimal-intel-v1.sh
+++ 130-install-xorg-minimal-intel-v1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-LIST_OF_APPS="xorg-minimal xorg-fonts xf86-video-intel xf86-input-synaptics xsetroot xbacklight gpm"
+LIST_OF_APPS="xorg-minimal xorg-fonts xf86-video-intel xf86-input-synaptics xsetroot xbacklight gpm xev"
 
 sudo xbps-install -S
 sudo xbps-install -Sy $LIST_OF_APPS
