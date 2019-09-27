a5995f79c7805b54cf56837eff63413b876b0c4b
khum0r <domagoj.kosmat@gmail.com>
2019-09-27 04:35:27 +0200

    Update 160-install-system-apps-v1.sh

--- 160-install-system-apps-v1.sh
+++ 160-install-system-apps-v1.sh
@@ -5,7 +5,7 @@ echo "#################################################"
 echo "Installing System Apps..."
 echo "#################################################"
 
-LIST_OF_APPS="setxkbmap xrdb lxappearance nitrogen neofetch inxi curl dmidecode glances gvfs hardinfo hddtemp htop lm_sensors mlocate net-tools numlockx screenFetch scrot sysstat tumbler vnstat wget unclutter nmap compton compton-conf arc-theme redshift ranger"
+LIST_OF_APPS="bash-completion setxkbmap xrdb lxappearance nitrogen neofetch inxi curl dmidecode glances gvfs hardinfo hddtemp htop lm_sensors mlocate net-tools numlockx screenFetch scrot sysstat tumbler vnstat wget unclutter nmap compton compton-conf arc-theme redshift ranger"
 
 sudo xbps-install -S
 sudo xbps-install -Sy $LIST_OF_APPS
