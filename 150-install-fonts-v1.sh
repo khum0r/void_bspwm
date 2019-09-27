67ec3e4d9ef7d5b61d19e7c99faa18b5f8d093ef
khum0r <domagoj.kosmat@gmail.com>
2019-09-27 04:30:07 +0200

    Update 150-install-fonts-v1.sh

--- 150-install-fonts-v1.sh
+++ 150-install-fonts-v1.sh
@@ -5,7 +5,7 @@ echo "#################################################"
 echo "Installing fonts..."
 echo "#################################################"
 
-LIST_OF_APPS="noto-fonts-ttf-extra google-fonts-ttf font-awesome font-iosevka font-mplus-ttf"
+LIST_OF_APPS="font-Siji noto-fonts-ttf-extra google-fonts-ttf font-awesome font-iosevka font-mplus-ttf"
 
 sudo xbps-install -S
 sudo xbps-install -Sy $LIST_OF_APPS
